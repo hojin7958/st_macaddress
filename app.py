1b1c0b0bc3ef48ea1746c70a6d5cd5804623cbc4
hojin7958 <imkoreanguy@gmail.com>
2023-11-16 08:21:46 +0900

    modified:   app.py 	new file:   requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import uuid
+from pywinauto import findwindows
+
+
 a = uuid.getnode()
 mac = ':'.join(("%012X" % a)[i:i+2] for i in range(0, 12, 2))
+
+
+for i in findwindows.find_elements():
+    print(i)
+
 
 st.write(mac)
